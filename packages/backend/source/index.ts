278e09cec40ddac3ff6c1b0216e68ed704de2069
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2022-11-07 13:12:34 +0200

    backend > dev: export

--- packages/backend/source/index.ts
+++ packages/backend/source/index.ts
@@ -1,11 +1,31 @@
 // #region imports
     // #region internal
+    import {
+        MotorControlOptions,
+    } from '~data/interfaces';
+
     import MotorControl from './objects/MotorControl';
     // #endregion internal
 // #endregion imports
 
 
 
+// #region module
+const MotorControlBackend = (
+    configuration: MotorControlOptions,
+) => {
+    const motorControl = new MotorControl(configuration);
+
+    return motorControl;
+}
+// #endregion module
+
+
+
 // #region exports
-export default MotorControl;
+export {
+    MotorControl,
+};
+
+export default MotorControlBackend;
 // #endregion exports
